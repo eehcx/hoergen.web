4ec700f9fff66f8fba384eff26f0f8db685051b7
eehcx <76226885+eehcx@users.noreply.github.com>
2025-07-14 18:13:19 -0600

    fix: SSR conversion to static for firebase hosting deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
-//import AstroPWA from '@vite-pwa/astro';
 
 export default defineConfig({
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://www.hoergen.com',
   compressHTML: true,
   integrations: [
-    sitemap(),
-    /*
-    AstroPWA({
-      manifest: {
-        name: 'Hoergen',
-        short_name: 'Hörgen',
-        description: 'Hoergen is a platform for sharing and discovering music.',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#121212',
-        icons: [
-          {
-            src: '/avatars/icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/avatars/icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['/.{js,css,html,png,jpg,svg,json}']
-      }
-    })
-    */
+    sitemap()
   ],
   i18n: {
     defaultLocale: 'es',
